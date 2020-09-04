b6a30079162f202efd55c9ecc0b9aeb02ec662b2
mira-ceti <ibellon@gmail.com>
2020-09-04 20:13:22 +0200

    Vistas y URLs

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    #return HttpResponse("Bienvenido a mi Blog")
+    return render(request, "blog/home.html")
+
