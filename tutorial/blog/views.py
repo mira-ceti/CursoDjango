0e689edbecd1ad8c99cb6741aa3a0de6641330cd
mira-ceti <ibellon@gmail.com>
2020-09-04 20:27:09 +0200

    Variables de contexto

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def home(request):
     #return HttpResponse("Bienvenido a mi Blog")
-    return render(request, "blog/home.html")
+    posts = Post.objects.all()
+    return render(request, "blog/home.html", {'posts': posts})
 
