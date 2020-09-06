899733cbc3b792069a2fd6569f0dcdd1c3896082
mira-ceti <ibellon@gmail.com>
2020-09-06 19:01:02 +0200

    Páginas dinámicas y Fin de Proyecto

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -8,3 +8,6 @@ def home(request):
     posts = Post.objects.all()
     return render(request, "blog/home.html", {'posts': posts})
 
+def post(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, "blog/post.html", {'post': post})
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import home
+from blog.views import home, post
 
 urlpatterns = [
     path('', home),
+    path('blog/<id>', post),
     path('admin/', admin.site.urls),
 ]
